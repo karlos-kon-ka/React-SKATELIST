bbd09d686526806e0cdfda7cc84ceab4f415ce60
Karl <143749395+karlos-kon-ka@users.noreply.github.com>
2024-06-04 01:23:44 +0200

    Actualizar Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -9,7 +9,7 @@ function Logo() {
 
       <h1 className="titulo">Lista de trucos</h1>
       <p  className="  bg-dark text-light descripcion">
-        ¡Prepára tus <span>SESSIONS</span> con tu lista de <span>TRUCOS</span>! <br/>
+        ¡Prepara tu <span>SESSION</span> con tu lista de <span>TRUCOS</span>! <br/>
         Añade a la lista los trucos que vas a hacer hoy y <span>¡COMPLETALOS!</span>.
       </p>
     </div>
